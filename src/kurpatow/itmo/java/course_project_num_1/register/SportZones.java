437b5c361c761626cc839075c62233330a679db8
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-30 13:52:30 +0300

    add fix course work

--- src/kurpatow/itmo/java/course_project_num_1/register/SportZones.java
+++ src/kurpatow/itmo/java/course_project_num_1/register/SportZones.java
@@ -2,9 +2,44 @@ package kurpatow.itmo.java.course_project_num_1.register;
 
 import  kurpatow.itmo.java.course_project_num_1.sportClub.Members;
 
-public enum SportZones {
-    SWIMMING_POOL (new PassType[] {PassType.ONE_PASS, PassType.FULL_PASS}, "Зона: Бассейн"),
-    GROUP_SPORTS (new PassType[] {PassType.DAY_PASS, PassType.FULL_PASS}, "Зона: Групповые спортивные занятия"),
-    GYM (new PassType[] {PassType.ONE_PASS, PassType.DAY_PASS, PassType.FULL_PASS}, "Зона: Тренажерный зал");
+import java.util.Locale;
 
+public enum SportZones {
+    SWIMMING_POOL (new PassType[] {PassType.ONE_PASS, PassType.FULL_PASS},
+            "Зона: Бассейн"),
+    GROUP_SPORTS (new PassType[] {PassType.DAY_PASS, PassType.FULL_PASS},
+            "Зона: Групповые спортивные занятия"),
+    GYM (new PassType[] {PassType.ONE_PASS, PassType.DAY_PASS, PassType.FULL_PASS},
+            "Зона: Тренажерный зал");
+
+    private PassType[] validMember;
+    private String sportClubZoneName;
+
+    SportZones(PassType[] validMember, String sportClubZoneName) {
+        this.validMember = new PassType[validMember.length];
+        for (int i = 0; i < validMember.length; i++) {
+            this.validMember[i] = validMember[i];
+        }
+        this.sportClubZoneName = sportClubZoneName;
+    }
+    public String getSportClubZoneName() {
+        return sportClubZoneName;
+    }
+    public static SportZones getSportZonesByZoneName(String sportClubZoneName) {
+        for (SportZones zone : SportZones.values()) {
+            if (sportClubZoneName.toLowerCase().equals(zone.sportClubZoneName.toLowerCase())) {
+                return zone;
+            }
+        }
+        return null;
+    }
+    // Проверяем, есть доступ в зону по абонементу или нет
+    public boolean access (Members members) {
+        for (PassType passType : validMember) {
+            if (passType.equals(members.getPassType())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/kurpatow/itmo/java/course_project_num_1/sportClub/SportClub.java
+++ src/kurpatow/itmo/java/course_project_num_1/sportClub/SportClub.java
@@ -1,4 +1,39 @@
 package kurpatow.itmo.java.course_project_num_1.sportClub;
 
+import kurpatow.itmo.java.course_project_num_1.register.SportZones;
+import java.time.LocalTime;
+import java.time.LocalDate;
+
 public class SportClub {
+
+    private Members[] swimmingPollMembers;
+    private Members[] groupSportsMembers;
+    private Members[] gymMembers;
+    
+    private LocalDate workingDate;
+
+    public SportClub() {
+        clubActivation();
+    }
+    private void setWorkingDate(LocalDate workingDate) {
+        if (workingDate == null) throw new IllegalArgumentException(
+                "Ошибка! Необходимо указать корректную даты начала работы!");
+    }
+    private void clubActivation() {
+        workingDate = null;
+        swimmingPollMembers = new Members[20];
+        groupSportsMembers = new Members[20];
+        gymMembers = new Members[20];
+    }
+    public void startWorking(LocalDate workingDate) {
+        System.out.println("Начало рабочего дня! " + "Дата: " + workingDate);
+        setWorkingDate(workingDate);
+    }
+    public void endWorking() {
+        System.out.println("Конец рабочего дня! " + "Дата: " + workingDate);
+        clubActivation();
+    }
+    private boolean sportClubWorking() {
+        return workingDate != null;
+    }
 }
