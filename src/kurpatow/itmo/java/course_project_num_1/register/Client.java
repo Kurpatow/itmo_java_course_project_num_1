89741bb3d9802789a7b76043868d3685c13a62d0
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-11-13 23:59:37 +0300

    project crush

--- src/kurpatow/itmo/java/course_project_num_1/register/Client.java
+++ src/kurpatow/itmo/java/course_project_num_1/register/Client.java
@@ -7,34 +7,34 @@ public class Client {
     private String secondName;
     private int dateOfBirth;
 
-        public void setFirstName (String firstName){
-            if (firstName.length() < 3)
-                throw new IllegalArgumentException(
-                        "Ошибка! При регистрации абонемента ИМЯ держателя карты должно состоять минимум из трёх букв");
-            this.firstName = firstName;
-        }
+    public void setFirstName (String firstName){
+        if (firstName.length() < 3)
+            throw new IllegalArgumentException(
+                    "Ошибка! При регистрации абонемента ИМЯ держателя карты должно состоять минимум из трёх букв");
+        this.firstName = firstName;
+    }
 
-        public void setSecondName (String secondName){
-            if (secondName.length() < 3)
-                throw new IllegalArgumentException(
-                        "Ошибка! При регистрации абонемента ФАМИЛИЯ держателя карты должна состоять минимум из трёх букв");
-            this.secondName = secondName;
-        }
+    public void setSecondName (String secondName){
+        if (secondName.length() < 3)
+            throw new IllegalArgumentException(
+                    "Ошибка! При регистрации абонемента ФАМИЛИЯ держателя карты должна состоять минимум из трёх букв");
+        this.secondName = secondName;
+    }
 
-        public void setDateOfBirth ( int dateOfBirth){
-            int currentYear = LocalDate.now().getYear();
-            if (currentYear - dateOfBirth > 80 || currentYear - dateOfBirth < 0)
-                throw new IllegalArgumentException("Ошибка! Проверьте корректно ли введен год рождения.");
-            this.dateOfBirth = dateOfBirth;
-        }
+    public void setDateOfBirth ( int dateOfBirth){
+        int currentYear = LocalDate.now().getYear();
+        if (currentYear - dateOfBirth > 80 || currentYear - dateOfBirth < 0)
+            throw new IllegalArgumentException("Ошибка! Проверьте корректно ли введен год рождения.");
+        this.dateOfBirth = dateOfBirth;
+    }
 
-        public int getDateOfBirth () {
-            return dateOfBirth;
-        }
-        public String getSecondName () {
-            return secondName;
-        }
-        public String getFirstName () {
-            return firstName;
-        }
+    public int getDateOfBirth () {
+        return dateOfBirth;
+    }
+    public String getSecondName () {
+        return secondName;
+    }
+    public String getFirstName () {
+        return firstName;
+    }
     }
